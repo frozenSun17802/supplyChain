c2086b736d31c29ffa9ec559e3f547394a3cbd25
frozenSun17802 <frozenSun17802@protonmail.com>
2024-05-06 23:16:08 +0300

    improve performance

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+
+import './App.css';
+import { Muibutton } from './component/Muibutton';
+import { Muiradio } from './component/Muiradio';
+import { Muirating } from './component/Muirating';
+import { Muiselect } from './component/Muiselect';
+import { Muiswitch } from './component/Muiswitch';
+import { Muitextfield } from './component/Muitextfield';
+//import { Typolology } from './component/Typolology';
+
+function App() {
+  return (
+    <div className="App">
+      {/*<Typolology/>*/}
+       {/*<Muibutton/>*/}
+       {/*<Muitextfield/>*/}
+      {/* <Muiselect/>*/}
+     {/* < <Muiradio/>*/}
+     {/* <<Muiswitch/>*/}
+     <Muirating/>
+    </div>
+  );
+}
+
+export default App;
